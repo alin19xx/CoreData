72bed3a4e96ca308eedb282ab6339632ec5bb98f
Alex Lin <alexlinsegarra@gmail.com>
2019-05-19 13:11:57 +0200

    Initial Commit

---
+++ CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData
+//
+//  Created by Alex Lin on 19/05/2019.
+//  Copyright © 2019 Alex Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
